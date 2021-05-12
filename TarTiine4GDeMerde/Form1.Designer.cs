1ca60ce5d70c35d4dd8fdaa4d70f2e01d641b960
Thibault <thibault@stantabcorp.com>
2021-05-12 16:23:28 +0200

    Ajoutez des fichiers projet.

---
+++ TarTiine4GDeMerde/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TarTiine4GDeMerde
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.clock = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Laisser cette fenêtre ouverte";
+            // 
+            // clock
+            // 
+            this.clock.Interval = 1000;
+            this.clock.Tick += new System.EventHandler(this.clock_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(171, 32);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(187, 71);
+            this.MinimumSize = new System.Drawing.Size(187, 71);
+            this.Name = "Form1";
+            this.Text = "4GDeMerde";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer clock;
+    }
+}
+
---
+++ TarTiine4GDeMerde/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TarTiine4GDeMerde
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            log = new EventLog("_4GDeMerde");
+            log.Source = "Service_4G";
+        }
+
+        private Boolean using4G = false;
+        private Boolean usingADSL = false;
+        private EventLog log;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            log.WriteEntry("Starting service", EventLogEntryType.Information);
+            clock.Start();
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            log.WriteEntry("Stopping service", EventLogEntryType.Information);
+            clock.Stop();
+        }
+
+        private void clock_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            DateTime start = new DateTime(now.Year, now.Month, now.Day, 20, 45, 0);
+            DateTime end = new DateTime(now.Year, now.Month, now.Day, 22, 15, 0);
+
+            if (start <= now && now <= end)
+            {
+                usingADSL = false;
+                if (!using4G)
+                {
+                    // Switch to 4G
+                    using4G = true;
+                    changeMetric(Properties.Settings.Default.interface_4g, 1);
+                    changeMetric(Properties.Settings.Default.interface_adsl, 2);
+                }
+
+            }
+            else
+            {
+                using4G = false;
+                if (!usingADSL)
+                {
+                    // Switch to ADSL
+                    usingADSL = true;
+                    changeMetric(Properties.Settings.Default.interface_4g, 2);
+                    changeMetric(Properties.Settings.Default.interface_adsl, 1);
+                }
+            }
+        }
+
+        private void changeMetric(String interfaceId, int metricValue)
+        {
+            ProcessStartInfo processStart = new ProcessStartInfo();
+            processStart.FileName = @"powershell.exe";
+            processStart.Arguments = "& {Set-NetIPInterface -InterfaceIndex \"" + interfaceId + "\" -InterfaceMetric \"" + metricValue + "\"}";
+            processStart.UseShellExecute = false;
+            processStart.CreateNoWindow = true;
+            processStart.RedirectStandardOutput = true;
+            processStart.RedirectStandardError = true;
+
+            Process process = new Process();
+            process.StartInfo = processStart;
+            process.Start();
+
+            process.WaitForExit();
+
+            log.WriteEntry(process.StandardOutput.ReadToEnd(), EventLogEntryType.Information);
+            log.WriteEntry(process.StandardError.ReadToEnd(), EventLogEntryType.Error);
+        }
+    }
+}
